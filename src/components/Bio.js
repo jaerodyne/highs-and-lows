3673a5f81b8fd74c354324a555d45e5cc6f1fb90
Jillian Somera <jsomera@gmail.com>
2018-02-25 14:17:52 -0600

    Fix typos

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Row, Col, Form, Button } from 'react-bootstrap';
 
 class Bio extends Component {
 	render() {
@@ -8,10 +7,10 @@ class Bio extends Component {
 				<div className="bio-text">
 					<h1>Bio</h1>
 					<p>
-						Jillian Somera and the Beta Option have come a long way since their inception in 2015. Founded by Jillian Somera, the band was initially a solo project for the singer-songwriter. Somera wrote and composed all her songs the guitar, taking musical influences from Amy Winehouse, Incubus, and John Mayer alike forging a sound that Jillian herself refers to as Alternative Rock/R&B.
+						Jillian Somera and the Beta Option has come a long way since their inception in 2015. Founded by Jillian Somera, the band was initially a solo project for the singer-songwriter. Somera wrote and composed all her songs on the guitar, taking musical influences from Amy Winehouse, Incubus, and John Mayer alike, forging a sound that Jillian herself refers to as Alternative Rock/R&B.
 					</p>
 					<p>
-						It wasn’t until her time at Columbia College Chicago’s music program that the project evolved into a band setting. Somera first enlisted her fellow classmate, Justin Rosado, to play bass for her EP release party. Their performance forged the start of Jillian Somera and the Beta Option and they began their search for the ever-elusive permanent drummer. After cycling through various drummers, they ultimately settled on fellow classmate Austin Deadman, completing the 3-piece outfit that they are today. 	
+						It wasn’t until her time at Columbia College Chicago’s music program that the project evolved into a band setting. Somera first enlisted her fellow classmate, Justin Rosado, to play bass for her EP release party. Their performance forged the start of Jillian Somera and the Beta Option and they began their search for the ever-elusive permanent drummer. After cycling through various drummers, they finally settled on fellow classmate Austin Deadman, completing the 3-piece outfit that they are today. 	
 					</p>
 					
 					<p>
