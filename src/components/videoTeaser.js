1abd9c7438777dd7aae5b134b897b8f3c7d6106b
Jillian Somera <jsomera@gmail.com>
2018-06-04 16:21:48 -0500

    Update html 5 video settings

--- src/components/videoTeaser.js
+++ src/components/videoTeaser.js
@@ -5,16 +5,17 @@ class VideoTeaser extends Component {
     super(props);
 
     this.state = {
-      videoURL: 'video/teaser.mp4'
+      videoMp4: 'video/teaser.mp4',
+      videoWebm: 'video/teaser.webm'
     }
   }
 
   render () {
     return (
       <div className="video-contain">
-        <video id="background-video" loop autoPlay>
-          <source src={this.state.videoURL} type="video/mp4" />
-          <source src={this.state.videoURL} type="video/ogg" />
+        <video id="background-video" loop autoPlay muted preload="auto" >
+          <source src={this.state.videoWebm} type="video/webm" />
+          <source src={this.state.videoMp4} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
