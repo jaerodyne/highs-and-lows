e3aed8eec68b8aa9722b8e90979e3fd801e2a846
Jillian Somera <jsomera@gmail.com>
2018-07-07 22:53:04 -0500

    Remove globe icon and add instagram icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
 			<div className="footer">
 				<ul>
 					<li><a href="https://www.facebook.com/jilliansomeramusic/"><FontAwesomeIcon icon={["fab", "facebook"]} /></a></li>
-					<li><a href="https://www.youtube.com/watch?v=-VtJDn4VXlc&list=PLzhsZ7gBote-41rL9qQU7bdAZo_Q6bPTd"><FontAwesomeIcon icon={["fab", "youtube"]} /></a></li>
+					<li><a href="https://www.instagram.com/jsbetaoption"><FontAwesomeIcon icon={["fab", "instagram"]}/></a></li>
 					<li><a href="https://open.spotify.com/artist/5sVYv8ruRxtVYloIOvcWqe"><FontAwesomeIcon icon={["fab", "spotify"]}/></a></li>
-					<li><a href="http://jilliansomera.com"><FontAwesomeIcon icon="globe"/></a></li>
+					<li><a href="https://www.youtube.com/watch?v=-VtJDn4VXlc&list=PLzhsZ7gBote-41rL9qQU7bdAZo_Q6bPTd"><FontAwesomeIcon icon={["fab", "youtube"]} /></a></li>
 				</ul>
 				<p className="copyright">&#169;2018 Jillian Somera</p>
 			</div>
