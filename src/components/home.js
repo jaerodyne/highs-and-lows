1824a1b0f364bcd025123107408515fbe56b150c
Jillian Somera <jsomera@gmail.com>
2018-09-08 22:24:24 -0500

    Add mobile streaming links

--- src/components/home.js
+++ src/components/home.js
@@ -412,6 +412,31 @@ class Home extends Component {
 							    	</div>
 									</Col>
 								</Row>
+	    	    		<h1>Listen Now</h1>
+								<Row>
+									<Col xs="12">
+										<iframe src="https://open.spotify.com/follow/1/?uri=spotify:artist:3qQ0Zv5eTJIbeAnSHyxXef?si=Z8ivcjfHTMKoPb0eRCx6wg&size=detail&theme=light" width="350" height="56" scrolling="no" frameBorder="0" style={{border:'none', marginLeft: '2em', overflow:'hidden'}} allowtransparency="true"></iframe>
+		    	    		</Col>
+		    	    	</Row>
+		    	    	<Row style={{marginTop: '1em'}}>
+									<Col xs={{ size: 4, offset: 1 }} style={{marginLeft: '3em', paddingLeft: '0', paddingRight: '0'}}>
+					    			<a href="http://bit.ly/highsandlows-googleplay">
+					    	  		<img src="../img/google-play-badge.png" alt="Listen on Google Play" width="100%" height="80%"/>
+					    	  	</a>
+						    	</Col>
+									<Col xs={{ size: 3}} style={{marginLeft: '0.8em', paddingRight: '15px'}}>
+						    		<a href="http://bit.ly/highsandlows-itunes">
+						      		<img src="../img/itunes-store-icon-blk.svg" alt="Get on iTunes" width="90%" height="80%"/>
+						      	</a>
+						    	</Col>
+									<Col xs="2" style={{padding: '0'}}>
+						    		<a href="http://bit.ly/highsandlows-applemusic">
+					    	  		<img src="../img/apple-music-icon-blk.svg" alt="Listen on Apple Music" width="90%" height="80%"/>
+						      	</a>
+						    	</Col>
+						    	<Col xs="2">
+						    	</Col>
+								</Row>
 								<Row>
 									<Col xs="12">
 										<Bio />
